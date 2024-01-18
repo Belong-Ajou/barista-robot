38bef28d8e7f3239e2dea6af437a4bda0e1434eb
mangoflap <ehdrjs625@naver.com>
2024-01-18 11:43:31 +0900

    modify for NTU

--- barista_robot/barista_basic.py
+++ barista_robot/barista_basic.py
@@ -17,26 +17,26 @@ class BaristaBasic(BaristaTemplate):
     def _rinse(self):
         rinse_action = RinseBehavior(SkipRinse())
         rinse_action.rinse()
-        self.robot.activate_gripper()
+        # self.robot.activate_gripper()
         return
 
     def _place_coffee_grounds(self):
         print("Place the coffee grounds in the filter paper.")
         self.robot.set_global_speed(50)
-        self.robot.set_speed(10.0)
+        self.robot.set_speed(15.0)
         home_point = conf.home_point
         cup_point = conf.grab_cup_point
         self.robot.move_PTP(cartesian_pose = home_point["P"], joint_pose= home_point["J"])
 
         cup_offset = (self.cup_location-1)*cup_point["CUP_X_OFFSET"]
         # Gripper - 그리퍼 벌리기 (Open the gripper)
-        self.robot.open_gripper()
+        # self.robot.open_gripper()
         # 1.PTP - 원두 컵 앞에 위치 (Locate in front of the cup of coffee beans)
         self.robot.move_cartesian(self._calculate_point(cup_point["P1"], cup_offset, 'x'))
         # 2.Linear - 원두 컵쪽으로 들어가기 (Move forward to the cup)
         self.robot.move_linear(cartesian_pose = self._calculate_point(cup_point["P2"], cup_offset, 'x'))
         # Gripper - 조여서 컵 잡기 (Close the gripper and grap the cup)
-        self.robot.close_gripper()
+        # self.robot.close_gripper()
         # 4.Linear - 잡은 뒤 컵 올리기 (Pick up the cup)
         self.robot.move_linear(cartesian_pose = self._calculate_point(cup_point["P1"], cup_offset, 'x'))
         
@@ -56,7 +56,7 @@ class BaristaBasic(BaristaTemplate):
         used_cup_point = conf.place_used_cup_point
         self.robot.move_cartesian(used_cup_point["P1"])
         self.robot.move_linear(cartesian_pose = used_cup_point["P2"])
-        self.robot.open_gripper()
+        # self.robot.open_gripper()
         sleep(1)
         self.robot.move_cartesian(cartesian_pose = used_cup_point["P1"])
 
@@ -67,9 +67,9 @@ class BaristaBasic(BaristaTemplate):
         # 케틀 잡기
         kettle_point = conf.grab_kettle_point
         self.robot.move_cartesian(kettle_point["P1"])
-        self.robot.open_gripper()
+        # self.robot.open_gripper()
         self.robot.move_linear(cartesian_pose = kettle_point["P2"])
-        self.robot.close_gripper()
+        # self.robot.close_gripper()
         sleep(1)
         self.robot.move_linear(cartesian_pose = kettle_point["P3"])
         self.robot.move_cartesian(kettle_point["P4"])
@@ -78,7 +78,7 @@ class BaristaBasic(BaristaTemplate):
 
     def _pour(self, time: int, amount: int):
         print("Pour: Pour in {0} grams of hot water, wait {1} seconds".format(str(amount), str(time)))
-        self.robot.run_program(conf.brewing_program["recipe1_2"])
+        # self.robot.run_program(conf.brewing_program["recipe1_2"])
         return
 
     def _wait(self, time:int):
@@ -91,7 +91,7 @@ class BaristaBasic(BaristaTemplate):
         self.robot.move_cartesian(kettle_point["P4"])
         self.robot.move_cartesian(kettle_point["P3"])
         self.robot.move_linear(cartesian_pose = kettle_point["P2"])
-        self.robot.open_gripper()
+        # self.robot.open_gripper()
         sleep(1)
         self.robot.move_linear(cartesian_pose = kettle_point["P1"])
         self.robot.move_cartesian(kettle_point["P4"])
@@ -128,13 +128,23 @@ if __name__ == "__main__":
     # mode = "Debug"
     mode = "Release"
     if mode == "Debug":
+        barista.robot.set_global_speed(40)
+        barista.robot.set_speed(10.0)
+        # barista.set_cup_location(1)
+        barista.set_dripper_location(1)
+        # barista._pour(1,1)
         # barista.robot.activate_gripper()
-        barista.robot.set_global_speed(50)
-        barista.robot.set_speed(5.0)
-        barista.set_cup_location(1)
-        barista.set_dripper_location(2)
-        barista._pour(1,1)
+        # sleep(1)
+        # barista.robot.close_gripper()
+        cup_point = conf.grab_kettle_point
+        barista.robot.move_cartesian(cup_point["P1"])
+        sleep(1)
+        barista.robot.move_linear(cartesian_pose = cup_point["P2"])
+        sleep(1)
+        barista.robot.move_linear(cartesian_pose = cup_point["P3"])
+        sleep(1)
+        barista.robot.move_cartesian(cup_point["P4"])
     else: 
-        barista.set_cup_location(1)
-        barista.set_dripper_location(2)
+        barista.set_cup_location(3)
+        barista.set_dripper_location(3)
         barista.make_coffee(my_recipe)
