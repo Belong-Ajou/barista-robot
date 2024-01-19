ba95845104bdc16092fdff195d92e86d1775aeb1
stevennedwinn <stevennedwinn@gmail.com>
2024-01-19 09:08:00 +0800

    Undistort and process depth image, add delay, and create depth array csv file

--- barista_robot/vision/camera_controller.py
+++ barista_robot/vision/camera_controller.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import os
+from defisheye import Defisheye
+import time
+from PIL import Image
 
 import pyk4a
 from pyk4a import Config, PyK4A
@@ -47,6 +50,82 @@ def main():
             color_img = capture.color[:, :, :3]
             depth_img = capture.depth
 
+            # Getting depth image array
+            np.set_printoptions(threshold=np.inf)
+            with open("depth_image.txt", "w") as file:
+                depth_img_str = np.array2string(depth_img, separator=',')
+                file.write(depth_img_str)
+            np.set_printoptions(threshold=1000)
+
+
+            dtype = 'linear'
+            format = 'fullframe'
+            fov = 180
+            pfov = 120
+
+            img_out = f"./defish_depth_array.jpg"
+
+            array_out = []
+
+            # for row in depth_img:
+            #     for column in row:
+            #         array_out.append([column, column, column])
+
+            # Reshape depth_img to have a third dimension of size 1
+            depth_img_reshaped = depth_img.reshape(depth_img.shape[0], depth_img.shape[1], 1)
+
+            # Replicate the values across the third dimension
+            array_out = np.repeat(depth_img_reshaped, 3, axis=2)
+
+            print("done array out")
+
+            depth_img = array_out
+
+            print("obj shape:", depth_img.shape)
+
+            obj = Defisheye(depth_img, dtype=dtype, format=format, fov=fov, pfov=pfov)
+
+            # To save image locally 
+            obj.convert(outfile=img_out)
+
+            # To use the converted image in memory
+            new_image = obj.convert()
+
+            # np.set_printoptions(threshold=np.inf)
+            # with open("obj_depth_image.txt", "w") as file:
+            #     obj_img_str = np.array2string(new_image, separator=',')
+            #     file.write(obj_img_str)
+            # np.set_printoptions(threshold=1000)
+
+            # print("done writing obj depth array")
+
+            # Cropping image
+            crop_coordinates = (175, 720, 45, 1020)  # Adjust these values as needed
+
+            # Crop the image using NumPy slicing
+            y_min, y_max, x_min, x_max = crop_coordinates
+            cropped_image = new_image[y_min:y_max, x_min:x_max]
+            print("cropped_image",cropped_image)
+            print("cropped_image.shape", cropped_image.shape)
+
+            # Resize the image using cv2.resize
+            # cv2.INTER_NEAREST is often used for depth images to avoid introducing new depth values through interpolation
+            resized_image = cv2.resize(cropped_image, (640, 640), interpolation=cv2.INTER_NEAREST)
+            print("resized_image:", resized_image)
+            print("resized_image.shape:", resized_image.shape)
+
+            # Select one channel from the 3D array. Assuming all channels have the same value, we can just take the first.
+            resized_image_2d = resized_image[:, :, 0]
+
+            resized_image_2d_colorized = colorize(resized_image_2d, (None, 10000), cv2.COLORMAP_HSV)
+
+            # Now save this 2D array to a CSV file, visualise and save the image too
+            np.savetxt("./real_depth_resized_image_array.csv", resized_image_2d, fmt='%d', delimiter=',')
+            cv2.imwrite('resized_image_2d_colorized.jpg', resized_image_2d_colorized)
+
+            # find bounding box
+            # bounding_boxes = detect_objects(captured_image)
+
             # 윈도우 창의 이름을 지정
             cv2.namedWindow('color', cv2.WINDOW_NORMAL)
             cv2.namedWindow('depth', cv2.WINDOW_NORMAL)
@@ -57,7 +136,12 @@ def main():
             
             # 이미지를 윈도우 창에 표시
             cv2.imshow('color', color_img)
-            cv2.imshow("depth", colorize(depth_img, (None, 5000), cv2.COLORMAP_HSV))
+
+            depth2_img = colorize(depth_img, (None, 5000), cv2.COLORMAP_HSV)
+            cv2.imshow("depth", depth2_img)
+
+            cv2.imwrite('color.jpg', color_img)
+            cv2.imwrite('depth.jpg', depth2_img)
             
             key = cv2.waitKey(1)
             if key == ord('q'):
@@ -72,9 +156,10 @@ def main():
 
                 fname2 = 'orgdepth_' + str(idx) + '.png'
                 out_2 = os.path.join(d_path, fname2)
-                cv2.imwrite(out_2, depth_img)
+                cv2.imwrite(out_2, depth2_img)
                 
                 idx += 1
+        time.sleep(5)
             
     cv2.destroyAllWindows()
     k4a.stop()
